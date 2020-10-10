a6556aabc2d129a1f27a1b55c6f93a3608d1de8c
ovisannn <ovi.sanjaya11@gmail.com>
2020-10-10 13:57:16 +0700

    nyicil landing page gan

---
+++ js/animation.js
@@ -0,0 +1,14 @@
+$("document.onload").ready(function(){
+    $(".navbar__ham").on("click", function(){
+      $(".navbar__links").toggleClass("active");  
+    });
+  
+    $(".navbar__links a li").on("click", function(){
+      $(".navbar__links").removeClass("active");
+    });
+  });
+  
+  
+  
+  
+    
